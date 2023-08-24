1327c510b9589fd5d3388bb3e15aeb2a02eea76a
ahong75 <ahong75@gatech.edu>
2023-08-24 16:19:59 -0400

    Implemented api call

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,49 @@
 import './App.css';
+import { useState, useEffect } from 'react'
 
-function App() {
+interface Volunteer {
+  name: string;
+  avatar: string;
+  hero_project: string;
+  notes: string;
+  email: string;
+  phone: string;
+  rating: number;
+  status: boolean;
+  id: number;
+}
+const TableEntry: React.FC<{ volunteer: Volunteer }> = ({volunteer}) => {
   return (
-    <div className="App">
-      <p>Hello World!</p>
+    <div>
+      <p>{volunteer.name}</p>
+      <p>{volunteer.avatar}</p>
+      <p>{volunteer.hero_project}</p>
+      <p>{volunteer.notes}</p>
+      <p>{volunteer.email}</p>
+      <p>{volunteer.phone}</p>
+      <p>{volunteer.rating}</p>
+      <p>{volunteer.status}</p>
+    </div>
+  )
+}
+function App() {
+  const [volunteers, setVolunteers] = useState<Volunteer[]>([])
+  
+  useEffect(() => {
+    fetch('http://localhost:5001/api/bog/users')
+      .then(response => {
+        return response.json()
+      })
+      .then(initialVolunteers => {
+        setVolunteers(initialVolunteers)
+      })
+      .catch(error => {
+        console.error('Error fetching data', error)
+      })
+  }, [])
+  return (
+    <div>
+      {volunteers.map(volunteer => <TableEntry volunteer={volunteer}/>)}
     </div>
   );
 }
