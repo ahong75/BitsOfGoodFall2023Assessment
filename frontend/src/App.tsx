9cde1531827425235ddd8ea56c639a0f2c471b8a
ahong75 <ahong75@gatech.edu>
2023-08-24 17:07:00 -0400

    Added basic table

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
 import './App.css';
-import { useState, useEffect } from 'react'
+import { useState, useEffect } from 'react';
 
 interface Volunteer {
   name: string;
@@ -12,40 +12,60 @@ interface Volunteer {
   status: boolean;
   id: number;
 }
-const TableEntry: React.FC<{ volunteer: Volunteer }> = ({volunteer}) => {
+
+const TableEntry: React.FC<{ volunteer: Volunteer }> = ({ volunteer }) => {
   return (
-    <div>
-      <p>{volunteer.name}</p>
-      <p>{volunteer.avatar}</p>
-      <p>{volunteer.hero_project}</p>
-      <p>{volunteer.notes}</p>
-      <p>{volunteer.email}</p>
-      <p>{volunteer.phone}</p>
-      <p>{volunteer.rating}</p>
-      <p>{volunteer.status}</p>
-    </div>
-  )
-}
+    <tr>
+      <td>{volunteer.name}</td>
+      <td>{volunteer.avatar}</td>
+      <td>{volunteer.hero_project}</td>
+      <td>{volunteer.notes}</td>
+      <td>{volunteer.email}</td>
+      <td>{volunteer.phone}</td>
+      <td>{volunteer.rating}</td>
+      <td>{volunteer.status ? 'Active' : 'Inactive'}</td>
+    </tr>
+  );
+};
+
 function App() {
-  const [volunteers, setVolunteers] = useState<Volunteer[]>([])
-  
+  const [volunteers, setVolunteers] = useState<Volunteer[]>([]);
+
   useEffect(() => {
-    fetch('http://localhost:5001/api/bog/users')
-      .then(response => {
-        return response.json()
-      })
+    fetch('http://localhost:5000/api/bog/users')
+      .then(response => response.json())
       .then(initialVolunteers => {
-        setVolunteers(initialVolunteers)
+        setVolunteers(initialVolunteers);
       })
       .catch(error => {
-        console.error('Error fetching data', error)
-      })
-  }, [])
+        console.error('Error fetching data', error);
+      });
+  }, []);
+
   return (
     <div>
-      {volunteers.map(volunteer => <TableEntry volunteer={volunteer}/>)}
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Avatar</th>
+            <th>Hero Project</th>
+            <th>Notes</th>
+            <th>Email</th>
+            <th>Phone</th>
+            <th>Rating</th>
+            <th>Status</th>
+          </tr>
+        </thead>
+        <tbody>
+          {volunteers.map(volunteer => (
+            <TableEntry key={volunteer.id} volunteer={volunteer} />
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
 
 export default App;
+
