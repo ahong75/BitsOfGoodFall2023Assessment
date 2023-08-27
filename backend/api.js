881bb9a74009c653e68c615affdb05799715811a
ahong75 <ahong75@gatech.edu>
2023-08-27 02:54:32 -0400

    Made database operations persistent

--- backend/api.js
+++ backend/api.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { database } from './database.js';
+import { database, databaseUpdate, databaseDelete } from './database.js';
 import cors from "cors";
 
 const app = express();
@@ -12,16 +12,41 @@ app.get('/', (req, res) => {
 });
 
 app.get('/api/bog/users', (req, res) => {
-    res.json(database).status(200);
+  res.json(database).status(200);
 });
 
 
 app.get('/api/bog/users/:id', (req, res) => {
-    const user = database.filter((user) => user.id === req.params.id)[0]
-    res.json(user).status(200)
+  const user = database.filter((user) => user.id === req.params.id)[0]
+  res.json(user).status(200)
 });
 
+app.put('/api/bog/users/:id', (req, res) => {
+  const newUser = req.params
+  const index = database.findIndex((user) => user.id === newUser.id)
+  if (index === -1) {
+    res.json({error: 'Volunteer not found'}).status(400)
+    return;
+  }
+  databaseUpdate(newUser.id, newUser)
+  res.json(newUser).status(200)
+});
 
+app.post('/api/bog/users', (req, res) => {
+  database = database.push(req.params)
+  res.json(req.params).status(201)
+});
+
+app.delete('/api/bog/users/:id', (req, res) => {
+  const id = req.params.id
+  const index = database.findIndex((user) => user.id === id)
+  if (index === -1) {
+    res.json({error: 'Volunteer not found'}).status(400)
+    return;
+  }
+  databaseDelete(id)
+  res.json('Volunteer successfully deleted').status(200)
+});
 // Start the server
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}`);
